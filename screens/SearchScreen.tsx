707b8f226e35fb67a51a38da42671143031f9783
Anatoli Rak <Rack.anatoly@gmail.com>
2023-06-15 14:50:58 +0200

    Create fletlist with buttons

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -11,14 +11,19 @@ import { Screen } from "../components/Screen";
 import { Button, Text } from "@ui-kitten/components";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { theme } from "../theme";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+
 const LISTMARGIN = 10;
 const WIDTH = Dimensions.get("screen").width - LISTMARGIN * 2;
+
 export const SearchScreen = () => {
-  const property = {
+  const properties = {
     images: [
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3FID%3DOIP.P4DMJbCaao_dpIs5dCb6IgHaLH%26pid%3DApi&f=1",
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.iE7mcw3w2aFFDhXP9A1lggHaE8%26pid%3DApi&f=1"
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.iE7mcw3w2aFFDhXP9A1lggHaE8%26pid%3DApi&f=1",
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.sN1pVaQ7SMfmzIydnPSKcgHaH1%26pid%3DApi&f=1",
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Q5Eunmn9ENDDwvQPZBCRdwHaE7%26pid%3DApi&f=1",
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Oe74GIp-Ini-tIVYe0bH6wHaE7%26pid%3DApi&f=1"
     ],
     rentLow: 3750,
     rentHigh: 31054,
@@ -31,19 +36,49 @@ export const SearchScreen = () => {
     zip: 33137,
     tags: ["Parking"]
   };
+
   const flatListRef = useRef<FlatList | null>(null);
   const viewConfig = { viewAreaCoveragePercentThreshold: 95 };
+  const [activeIndex, setActiveIndex] = useState(0);
+  const onViewRef = useRef(({ changed }: { changed: any }) => {
+    if (changed[0].isViewable) {
+      setActiveIndex(changed[0].index);
+    }
+  });
+  const handlePressLeft = () => {
+    if (activeIndex === 0)
+      return flatListRef.current?.scrollToIndex({
+        animated: false,
+        index: properties.images.length - 1
+      });
+
+    flatListRef.current?.scrollToIndex({
+      index: activeIndex - 1
+    });
+  };
+  const handlePressRight = () => {
+    if (activeIndex === properties.images.length - 1)
+      return flatListRef.current?.scrollToIndex({
+        animated: false,
+        index: 0
+      });
+
+    flatListRef.current?.scrollToIndex({
+      index: activeIndex + 1
+    });
+  };
   return (
     <Screen style={{ marginHorizontal: LISTMARGIN }}>
       <View>
         <FlatList
           ref={(ref) => (flatListRef.current = ref)}
-          data={property.images}
+          data={properties.images}
           horizontal
           showsHorizontalScrollIndicator={false}
           snapToAlignment="center"
           pagingEnabled={true}
           viewabilityConfig={viewConfig}
+          onViewableItemsChanged={onViewRef.current}
           renderItem={({ item, index }) => (
             <Image
               source={{ uri: item }}
@@ -56,10 +91,16 @@ export const SearchScreen = () => {
           )}
           keyExtractor={(item) => item}
         />
-        <Pressable style={{ position: "absolute", top: 95, left: 5 }}>
+        <Pressable
+          style={{ position: "absolute", top: 95, left: 5 }}
+          onPress={handlePressLeft}
+        >
           <MaterialCommunityIcons name="chevron-left" color="white" size={45} />
         </Pressable>
-        <Pressable style={{ position: "absolute", top: 95, right: 5 }}>
+        <Pressable
+          style={{ position: "absolute", top: 95, right: 5 }}
+          onPress={handlePressRight}
+        >
           <MaterialCommunityIcons
             name="chevron-right"
             color="white"
@@ -79,8 +120,8 @@ export const SearchScreen = () => {
             style={{ flexDirection: "row", justifyContent: "space-between" }}
           >
             <Text category={"s1"}>
-              ${property.rentLow.toLocaleString()} -{" "}
-              {property.rentHigh.toLocaleString()}
+              ${properties.rentLow.toLocaleString()} -{" "}
+              {properties.rentHigh.toLocaleString()}
             </Text>
             <MaterialCommunityIcons
               name="heart-outline"
@@ -89,18 +130,18 @@ export const SearchScreen = () => {
             />
           </View>
           <Text category={"c1"}>
-            {property.bedroomLow}-{property.bedroomHigh} Beds
+            {properties.bedroomLow}-{properties.bedroomHigh} Beds
           </Text>
           <Text category={"c1"} style={{ marginTop: 5 }}>
-            {property.name}
+            {properties.name}
           </Text>
-          <Text category={"c1"}>{property.street}</Text>
+          <Text category={"c1"}>{properties.street}</Text>
           <Text category={"c1"}>
-            {property.city},{property.state},{property.zip}
+            {properties.city},{properties.state},{properties.zip}
           </Text>
           <Text category={"c1"} style={{ marginTop: 5 }}>
-            {property.tags.map((tag, index) =>
-              index === property.tags.length - 1 ? tag : `${tag}`
+            {properties.tags.map((tag, index) =>
+              index === properties.tags.length - 1 ? tag : `${tag}`
             )}
           </Text>
           <View
